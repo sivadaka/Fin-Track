e2523019cb01785bded9070560b14ffcf38175c5
sivadaka <sivakumarreddy256@gmail.com>
2024-08-25 09:15:53 +0530

    Fin-Track App setup

---
+++ src/test/java/com/fin/FinTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
